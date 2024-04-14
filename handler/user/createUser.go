76214ce8a052dff8c83b86b819fcaf44e722a48c
Eduardo Silva <eduardo@joycar.com.br>
2024-04-14 19:09:47 -0300

    cors and fix create user

--- handler/user/createUser.go
+++ handler/user/createUser.go
@@ -1,9 +1,11 @@
 package user
 
 import (
+	"errors"
 	"github.com/Eduardo681/go_routine/helper"
 	"github.com/Eduardo681/go_routine/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -30,6 +32,17 @@ func CreateUserHandler(ctx *gin.Context) {
 	if err := request.Validate(); err != nil {
 		logger.ErrF("validation error: %v", err.Error())
 		helper.SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	var existingUser schemas.User
+	if err := db.Where("email = ?", request.Email).First(&existingUser).Error; err == nil {
+		logger.Err("User already exists")
+		helper.SendError(ctx, http.StatusBadRequest, "User already exists with the same email")
+		return
+	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+		logger.ErrF("Error checking user existence: %s", err.Error())
+		helper.SendError(ctx, http.StatusInternalServerError, "Internal server error")
 		return
 	}
 
--- router/router.go
+++ router/router.go
@@ -1,11 +1,18 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Init() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Content-Type", "Authorization", "x-access-token"},
+	}))
 
 	initRoutes(router)
 
